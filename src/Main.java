d8d4899c403346731c41b74eb08ecdb54bb4c814
Pablo Ramos M <pablotec123@estudiantec.cr>
2021-03-24 17:21:51 -0600

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)  {
         try{
-            QuadraticEquation e1=new QuadraticEquation(1,0,3);
+            QuadraticEquation e1=new QuadraticEquation(1,0,3);//Aqui puede tirar un error entonces usamos el try y catch
             e1.QuadraticCalc();
         } catch (QuadraticException e) {
             e.printStackTrace();
@@ -9,7 +9,7 @@ public class Main {
             System.out.println("Exit System");
         }
 
-        Unchecked example2=new Unchecked();
+        Unchecked example2=new Unchecked();//Ejemplo de Unchecked
         example2.UncheckedMethod();
         FileReader2 example3=new FileReader2();
         example3.FileReader();
